772890ed494d5c7bed3c1d6ce8c5f337bc4e77ba
Constantin Dumitrescu <dum.constantin@gmail.com>
2014-05-27 14:44:05 +0300

    Removed comments.

--- main.js
+++ main.js
@@ -34,104 +34,9 @@
 (function () {
     "use strict";
 
-    var path = require('path');
-
-    var Layer = require('./lib/Layer.js')
-    require('./lib/Utils.js');
-    var Structure = require('./lib/Structure.js');
-
-    /**
-     * UI Component constructor.
-
-    function UIComponent() {
-
-    }
-
-    // () vs []
-    // Functions from conventions
-
-    UIComponent.prototype.setSiblings = function (setType, elements) {
-        var _this = this;
-
-        Object.keys(elements).forEach(function (elementName) {
-
-            if (undefined === _this.siblings[elementName] && 'list' === setType) {
-                _this.siblings[elementName] = [];
-            }
-
-            _this.parent.find('.' + elementName).forEach(function (result) {
-                var item = {};
-
-                item.element = result;
-
-                elements[elementName].forEach(function (elementSelector) {
-                    item[elementSelector] = result.find('.' + elementSelector)[0];
-                });
-
-                if ('list' === setType) {
-                    _this.siblings[elementName].push(item);
-                } else {
-                    _this.siblings[elementName] = item;
-                }
-            });
-
-        });
-    };
-
-
-    /**
-     * Slider constructor.
-     * Will register a layer and will modify the layer structure to conform
-     * to that of a slider/carusel syntax while maintaining the initial logic.
-
-    function Slider(layer) {
-
-        this.parent = layer;
-        this.siblings = {};
-
-        this.setSiblings('elements', {
-            arrows: ['left', 'right']
-        });
-
-        this.setSiblings('list', {
-            slides: ['title', 'caption', 'image'],
-            bullets: ['title'],
-            thumbnails: ['title', 'image']
-        });
-
-        console.log(Object.keys(this.siblings.arrows.left));
-
-        return this;
-    }
-    Slider.prototype = Object.create(UIComponent.prototype);
-    Slider.prototype.constructor = Slider;
-
-
-    // .slider {
-        html: '<div id="slider">' +
-                    '<div id="slides">{{slider_collection}}</div>' +
-                    '<div id="bullets">{{bullet_collection}}></div>' +
-                '</div>',
-        collections: {
-            slider: {
-                html: '<div class="slide"><h2>{{title}}</h2><img src="{{image}}" /></div>',
-                content: [
-                    { title: 'foo bar', image: 'foo.png'},
-                    { title: 'baz bar', image: 'baz.png'}
-                ],
-                params: {
-                    orderBy: 'date',
-                    order: 'asc',
-                    limit: 5
-                }
-            },
-            bullet: {
-                html: '<div class="bullet"><span>{{index}}</span></div>'
-            }
-        }
-    }
-    .slides.orderBy(date).order(asc).limit(5)
-*/
+    var path = require('path'),
+        require('./lib/Utils.js'),
+        Structure = require('./lib/Structure.js');
 
     /**
      * Call GeneratorX to parse the document and output the HTML and CSS files.
@@ -171,8 +76,6 @@
             generator: generator
         });
 
-        // structure.registerUiComponent(Slider);
-
         structure.events.on('imagesFinished', function () {
 
             structure
@@ -182,7 +85,6 @@
                 .saveStructureToJSON()
                 .refreshCode()
                 .generateLogicStructure()
-                // .parseUIStructure()
                 .outputCode();
                 // .outputToWordpress();
 
@@ -197,112 +99,6 @@
             .queueImagesForGeneration()
             .startImageGeneration();
     }
-
-    // Verificarea de float este simpla:
-    // - Daca un element incepe de la marginea unui container
-    // - Daca un element nu incepe de la marginea unui container dar are acceasi distanta
-    // de sus ca unul care incepe de la marginea unui container
-    // - Daca un element este intre alte elemente care au aceeasi distanta intre ele
-    // si unul dintre elemente incepe de la marginea unui container
-    // - 
-
-    /*
-    function findFloatables(layers, type) {
-        var flotables = [],
-            firstElementsInRow = [],
-            disposeFloatables = false,
-            marginColumn,
-            marginRow;
-
-        if (1 < layers.length) {
-
-            // Order layers by left alignment
-            layers.sort(function (left, right) {
-                return left.properties.parsedCSS.left > right.properties.parsedCSS.left;
-            });
-
-            // Order layers by top down alignment
-            layers.sort(function (left, right) {
-                return left.properties.parsedCSS.top > right.properties.parsedCSS.top;
-            });
-
-            // Clone array
-            layers.forEach(function (layer) {
-                flotables.push(layer);
-            });
-
-            /*
-            // Find the layers that have the same top
-            flotables = flotables.filter(function (layer) {
-                if (flotables[0].properties.parsedCSS.top === layer.properties.parsedCSS.top) {
-                    return true;
-                }
-            });
-           
-
-            // Calculate the necessary margin for all floated layers
-            marginColumn =  flotables[1].properties.parsedCSS.left - 
-                    (flotables[0].properties.parsedCSS.left +
-                    parseInt(flotables[0].properties.parsedCSS.width));
-
-            console.log("MarginColumn: " + marginColumn);
-            // The first element of each row must not have margins
-            firstElementsInRow.push(flotables[0].id);
-            console.log(firstElementsInRow);
-
-            // Find the margin between other layers
-            flotables = flotables.filter(function (layer, index) {
-                var prev = flotables[index - 1],
-                    value;
-
-                // If a flotable candidate did not conform with the margin
-                // then all following canditates are also not floated.
-                if (true === disposeFloatables) {
-                    return false;
-                }
-
-                if (undefined !== prev) {
-                    // If floated cantidate is on a different row.
-                    if (prev.properties.parsedCSS.top !== layer.properties.parsedCSS.top) {
-                        firstElementsInRow.push(layer.id);
-
-                        // Calculate the bottom margin of the above row
-                        marginRow = layer.properties.parsedCSS.top -
-                            (parseInt(flotables[0].properties.parsedCSS.height) +
-                            flotables[0].properties.parsedCSS.top);
-
-                        return true;
-                    }
-
-                    value = 
-                        layer.properties.parsedCSS.left - 
-                        (prev.properties.parsedCSS.left +
-                        parseInt(prev.properties.parsedCSS.width));
-
-                    console.log(layer.properties.parsedCSS.left + " vs " + prev.properties.parsedCSS.left + " vs " + prev.properties.parsedCSS.width);
-                    console.log("Value: " + value);
-                    if (value === marginColumn) {
-                        return true;
-                    } else {
-                        disposeFloatables = true;
-                        return false;
-                    }
-                } else {
-                    return true; // The first floated element which is also reference.
-                }
-            });
-
-            console.log('Found ' + flotables.length + ' ' + type + ' floatable elements.');
-        }
-
-        return {
-            elements: flotables,
-            marginColumn: marginColumn,
-            marginRow: marginRow,
-            firstElementsInRow: firstElementsInRow
-        };
-    }*/
-
 
     // Init
     exports.init = function (generator) {
