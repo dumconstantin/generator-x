32ffdd9c987c829935d90fe566d960c1f6c9a8a0
Daniel Teodoroiu <daniel.teodoroiu@gmail.com>
2014-06-04 18:08:49 +0300

    Integration final structure implementation

---
+++ plugins/DefaultPlugin.js
@@ -0,0 +1,57 @@
+function DefaultPlugin(layer) {
+    this.layer = layer;
+    var _this = this;
+
+    this.elements = {
+        arrowLeft: {
+            background: 'image'
+        }
+    }
+
+    this.integration = {
+        html: '',
+        elements: {
+
+//            thumbnail: {
+//                html: '<div class="arrowLeft"><img src="#"/><span>{{ title }}</span>{% comments %}</div>',
+//                content: {
+//                    src: '',
+//                    title: ''
+//                }
+//            }
+
+        },
+        plugins: {
+        }
+
+//        content: {
+//
+//        }
+    }
+}
+
+DefaultPlugin.prototype.getIntegration = function() {
+
+    var _this = this;
+
+    var integration = this.layer.getIntegration(this.elements);
+
+    this.integration.html = integration.html;
+    this.integration.elements = integration.elements;
+
+    Object.keys(integration.plugins).forEach(function(pluginName){
+
+        var layer = integration.plugins[pluginName];
+
+        eval('var '+layer.convention.plugin.name+' = require("'+layer.convention.plugin.path+'");');
+        eval('layer.plugin = new '+layer.convention.plugin.name+'(layer);');
+
+        _this.integration.plugins[pluginName] = layer.plugin.getIntegration();
+
+    });
+
+    return this.integration;
+
+};
+
+module.exports = DefaultPlugin;
