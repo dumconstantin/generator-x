f3b5abdb9d236e2926f439ece3af10a6056b69b5
Constantin Dumitrescu <dinu@brandup.ro>
2014-06-26 11:29:34 +0300

    Added tests for getTopElementIndex.

--- spec/spec/Grid.js
+++ spec/spec/Grid.js
@@ -79,6 +79,31 @@ describe('Grid', function() {
 
     });
 
+    describe('getTopElementIndex', function () {
+
+        it('should find the highest element if the element is first', function () {
+            var elements = [{ top: -10 }, { top: 5 }];
+
+            expect(getTopElementIndex(elements)).toBe(0);
+        });
+
+
+        it('should find the highest element if the element is last', function () {
+            var elements = [{ top: 100 }, { top: 5 }];
+
+            expect(getTopElementIndex(elements)).toBe(1);
+        });
+
+
+        it('should find the first element in a list with equal elements', function () {
+            var elements = [{ top: 100 }, { top: 10 }, { top: 10 }];
+
+            expect(getTopElementIndex(elements)).toBe(1);
+        });
+
+    });
+
+
     describe('row detection', function () {
 
         it('should find elements that have the same top', function() {
@@ -162,6 +187,7 @@ describe('Grid', function() {
         });
 
     });
+
 
 
 });
