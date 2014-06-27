c33b85231352324c851dfd17eb9a5487d7352146
Constantin Dumitrescu <dum.constantin@gmail.com>
2014-06-27 13:01:48 +0300

    Added tests for the detectIntersectionElements function.

--- spec/Grid_test.js
+++ spec/Grid_test.js
@@ -222,7 +222,6 @@ describe('Grid', function() {
 
     });
 
-
     describe('isValidBoundries', function () {
 
         it('should validate an element with top, left, width, height', function () {
@@ -382,7 +381,44 @@ describe('Grid', function() {
         });
 
     });
+     
+    describe('detectIntersectionElements', function () {
 
+        it('should add to a row the intersection element that has its top within the row boundries', function () {
+            var elements = createGrid(1, 4),
+                element,
+                row;
+
+            elements.push({
+                id: 5,
+                top: 20,
+                left: 120,
+                width: 50,
+                height: 400
+            });
+
+            elements = augmentElements(elements);
+
+            showGrid(elements);
+            row = detectRow(elements);
+            row = detectIntersectionElements(row, elements);
+
+            row.children.every(function (child) {
+                if (undefined !== child.intersectionElement) {
+                    element = child;
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+
+            expect(element).toBeDefined();
+            expect(element.height).toBe(30);
+            expect(element.width).toBe(50);
+            expect(element.intersectionElement.id).toBe(5);
+        });
+
+    });
 
     describe('detectComposedElements', function () {
 
@@ -447,5 +483,5 @@ describe('Grid', function() {
         });
 
     });
-     
+
 });
