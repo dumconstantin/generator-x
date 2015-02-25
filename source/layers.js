dfc5c2df8779e8cde73d2c7950fce0f6123144b2
Constantin Dumitrescu <dum.constantin@gmail.com>
2015-02-26 01:39:14 +0200

    Remove closure.

--- source/layers.js
+++ source/layers.js
@@ -1,20 +1,17 @@
-(function () {
-	"use strict"
+"use strict"
 
-	function getStyles(documentLayer) {
-		
+function getStyles(documentLayer) {
+	
+}
+
+function createLayer(documentLayer) {
+	return {
+		documentLayer: documentLayer
+		, children: getLayers(documentLayer)
+		, styles: getStyles(documentLayer)
 	}
+}
 
-	function createLayer(documentLayer) {
-		return {
-			documentLayer: documentLayer
-			, children: getLayers(documentLayer)
-			, styles: getStyles(documentLayer)
-		}
-	}
-
-	exports.create = function createLayers(documentLayers) {
-		return (true === documentLayers instanceof Array) ? documentLayers.map(createLayer) : []
-	}
-
-}())
+exports.create = function createLayers(documentLayers) {
+	return (true === documentLayers instanceof Array) ? documentLayers.map(createLayer) : []
+}
