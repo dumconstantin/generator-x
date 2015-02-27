7eb208eaf4c55f999dc70944cb53fda872ff9f2d
Constantin Dumitrescu <dinu@brandup.ro>
2015-02-27 03:27:40 +0200

    Simplify project creation.

--- source/project.js
+++ source/project.js
@@ -1,44 +1,20 @@
-"use strict"
+'use strict'
 
-var fs = require("fs")
-	, path = require("path")
-	, slash = require("slash")
-
-// All plurals means an array
+var fs = require('fs')
+	, path = require('path')
+	, slash = require('slash')
 
 function getFolderPath(document) {
-	return path.resolve(__dirname, "../", "generated", getProjectName(document))
+	return path.resolve(__dirname, '../', 'generated', getProjectName(document))
 }
 
 function getProjectName(document) {
-	return path.basename(slash(document.file), ".psd")
+	return path.basename(slash(document.file), '.psd')
 }
 
-function saveDocument(document) {
-	fs.writeFileSync(
-		path.resolve(getFolderPath(document), "document.json")
-		, JSON.stringify(document, null, 4)
-	)
-}
+module.exports = function (document) {
+	var save = require('./save.js') 
 
-function createFolder(folderPath) {
-	try {
-		fs.mkdirSync(folderPath)
-	} catch (error) {
-		if ('EEXIST' !== error.code)
-			throw error
-	}
-}
-
-exports.save = function saveProject(document) {
-	createFolder(getFolderPath(document))
-	saveDocument(document)
-	return this
-}
-
-exports.build = function buildProject(document) {
-	var layers = require("./source/layers.js")
-
-	layers.create(document.layers)
-	return this
+	save.json(path.resolve(getFolderPath(document), 'source', 'document.json'), document)
+	save.json(path.resolve(getFolderPath(document), 'source', 'tree.json'), require('./makeTree.js')(document.layers))
 }
