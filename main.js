24ffe517bc95f1207977ecf767d84048853b129e
dumconstantin <dinu@brandup.ro>
2015-03-12 12:25:57 +0200

    Change generator usage by implementing the generator interface.

--- main.js
+++ main.js
@@ -5,13 +5,4 @@ function start(document) {
   require('./source/project.js')(Object.freeze(document))
 }
 
-function error(error) {
-  console.error(err)
-}
-
-require('generator-core')(function (generator) {
-  generator
-    .getDocumentInfo()
-    .then(start, error)
-    .done()
-})
+require('./source/generatorInterface.js').getDocumentPromise().done(start)
