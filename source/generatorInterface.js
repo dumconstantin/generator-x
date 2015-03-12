422fe4080a5117fcb808735c253380aa29318cef
dumconstantin <dinu@brandup.ro>
2015-03-12 12:25:26 +0200

    Implement the getDocumentInfo method. API: getDocumentPromise

--- source/generatorInterface.js
+++ source/generatorInterface.js
@@ -1,17 +1,22 @@
 'use strict'
-var path = require('path')
-    , generator = require(path.resolve(__dirname, '../', 'node_modules/generator-core/lib/generator.js'))
-    , config = {
+var when = require('when')
+
+function generatorPromise() {
+    var generator = require('../node_modules/generator-core/lib/generator.js').createGenerator() 
+
+    generator.start({
         port: 49494
         , hostname: 'localhost'
         , password: '123456'
-    }
+    })
     
-generator.createGenerator().start(config).done(function () {
-    console.log('Loaded generator')
-    generator.getDocumentInfo()
-    .then(function () {
-      console.log('Foobar')
-    }, console.log('barfoo'))
-})
+    return generator
+}
 
+function documentPromise() {
+    return generatorPromise().getDocumentInfo()
+}
+
+module.exports = {
+    getDocumentPromise: documentPromise
+}
