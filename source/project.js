a6fc8c8bf319c0bfe132664b9459d2b6f6273736
dumconstantin <dinu@brandup.ro>
2015-03-13 11:14:27 +0200

    Add createProjectDocument function.

--- source/project.js
+++ source/project.js
@@ -3,20 +3,32 @@
 var fs = require('fs')
 	, path = require('path')
 	, slash = require('slash')
+    , R = require('ramda')
+    , U = require('../libs/utils.js')
 
 function getFolderPath(document) {
-	return path.resolve(__dirname, '../', 'generated', getProjectName(document))
+	return path.resolve(__dirname, '..', 'generated', getProjectName(document))
 }
 
 function getProjectName(document) {
 	return path.basename(slash(document.file), '.psd')
 }
 
+function createProjectDocument(document) {
+    return R.pipe(
+        U.setProp('uid', require('node-uuid').v1()) 
+    )(document)
+}
+
 function createProject(document) {
 	var save = require('./save.js') 
+        , project = createProjectDocument(document)
+    
+    save.json(path.resolve(getFolderPath(document), 'source', 'document.json'), document)
+    save.json(path.resolve(getFolderPath(document), 'source', 'project.json'), project)
+    save.json(path.resolve(getFolderPath(document), 'source', 'tree.json'), require('./makeTree.js')(project))
 
-	save.json(path.resolve(getFolderPath(document), 'source', 'document.json'), document)
-	save.json(path.resolve(getFolderPath(document), 'source', 'tree.json'), require('./makeTree.js')(document))
+    return project
 }
 
 module.exports = createProject
