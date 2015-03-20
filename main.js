c2563691317f9419671c115a175766304f289736
dumconstantin <dinu@brandup.ro>
2015-03-20 10:56:28 +0200

    Modify project api call.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // Starts the system using the exported document from Generator Core
 function start(document) {
-  require('./source/project.js')(Object.freeze(document))
+  require('./source/project.js').make(document)
 }
 
 require('./source/generatorInterface.js').documentP().done(start)
