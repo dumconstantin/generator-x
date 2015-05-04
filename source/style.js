01eae1ed8bfd7515009cbecba605d0f196b68e0f
dumconstantin <dinu@brandup.ro>
2015-05-04 07:52:01 +0300

    Move deriveStyles to its own style.js file.

---
+++ source/style.js
@@ -0,0 +1,46 @@
+'use strict'
+
+var style = {}
+
+style.background = R.curry(function (document, layer) {
+    var style = {}
+
+    style.color = {
+        red: U.getProp(255, 'fill.color.red', layer)
+            , green: U.getProp(255, 'fill.color.green', layer)
+            , blue: U.getProp(255, 'fill.color.blue', layer)
+    }
+
+    return style
+})
+
+style.dimensions = R.curry(function (document, layer) {
+    var style = {}
+
+    return style
+})
+
+style.border = R.curry(function (document, layer) {
+    var style = {}
+
+    return style
+})
+
+style.shadow = R.curry(function (document, layer) {
+    var style = {}
+
+    return style
+})
+
+module.exports = {}
+
+Object.keys(style).forEach(function (method) {
+    module.exports[method] = style[method]
+})
+
+module.exports.all = R.curry(function (document, layer) {
+    return Object.keys(style).reduce(function (aggregator, method) {
+        aggregator[method] = style[method](document, layer)
+        return aggregator
+    }, {})
+})
